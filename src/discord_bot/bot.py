81b04722b70b42d90dde2e98559b4b248f149cf0
Andrea Kim <80376288+jinmisul@users.noreply.github.com>
2023-03-25 14:31:32 -0400

    im confused

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -11,6 +11,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord! Hi')
 
 client.run(TOKEN)
