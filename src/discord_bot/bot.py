6d5fd077d0507cbbcc85e343020ae56ee6749b0d
Andrea Kim <80376288+jinmisul@users.noreply.github.com>
2023-03-25 14:33:12 -0400

    burh

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -11,6 +11,6 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord! Hi')
+    print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
